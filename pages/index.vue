0306769d7fab8c109af831ad6df74a0b580986f3
jim <jimmyy5121@gmail.com>
2024-03-08 21:23:55 +0800

    header

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>this is home</div>
+  <!-- <div>this is home</div> -->
+  <!-- <div class="grid min-h-screen place-content-center">
+    <div
+      class="grid h-52 w-52 place-content-center rounded-full bg-emerald-500 shadow-2xl"
+    >
+      <div class="grid h-32 w-32 place-content-center rounded-full bg-teal-200">
+        <div
+          class="grid h-16 w-16 place-content-center rounded-full bg-red-400"
+        ></div>
+      </div>
+    </div>
+  </div> -->
+  <div class="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+    <div class="sticky top-0 z-10 bg-teal-700 text-white">
+      <div class="mx-auto flex max-w-4xl items-center justify-between p-4">
+        <div class="text-3xl font-medium">🚀 Jim Rockets</div>
+        <div class="cursor-pointer text-3xl sm:hidden">&#9776;</div>
+      </div>
+    </div>
+  </div>
 </template>
